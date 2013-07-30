54869a06805551cbb4525b8e2a5986d69c0cb621
Sina Masoud-Ansari <s.ansari@auckland.ac.nz>
2013-07-30 13:53:55 +1200

    Model offset

--- ModelSpace.py
+++ ModelSpace.py
@@ -2,6 +2,10 @@
 Classes and methods for representing neurons in some spatial configuration
 
 Author: Sina Masoud-Ansari
+
+Classes:
+	Point3D
+	Cuboid3D
 """
 
 import math
@@ -21,28 +25,33 @@ class Point3D:
 	def __ne__(self, other):
 		return not self.__eq__(other)	
 
+def distance3D(i, j):
+	"""Distance between two 3D euclidean points"""
+	dx = i.x - j.x
+	dy = i.y - j.y
+	dz = i.z - j.z
+	return math.sqrt(dx*dx + dy*dy + dz*dz)
+
+
 class Cuboid3D:
 	"""Class for 3D Euclidean space"""
-	def __init__(self, x, y, z):
+	def __init__(self, x, y, z, offset=Point3D(0,0,0)):
 		self.x = x
 		self.y = y
 		self.z = z
+		self.size = x * y * z
+		self.offset = offset
 
-	def pointFrom1D(self, p):
+	def pointFrom1D(self, i):
 		"""Find the 3D coordinates of a 1D point"""
-		x = p % self.x
-		y = (p / self.x) % self.y
-		z = p / (self.x * self.y)
-		return Point3D(x, y, z)
-
-	def distance (self, i, j):
-		"""Find the distance between two 3D points"""
-		pi = self.pointFrom1D(i)
-		pj = self.pointFrom1D(j)
-		dx = pi.x - pj.x
-		dy = pi.y - pj.y
-		dz = pi.z - pj.z
-		return math.sqrt(dx*dx + dy*dy + dz*dz)
+		x = i % self.x
+		y = (i / self.x) % self.y
+		z = i / (self.x * self.y)
+		ox = self.offset.x
+		oy = self.offset.y
+		oz = self.offset.z
+		return Point3D(x+ox, y+oy, z+oz)
+		
 
 """
 Unit Tests
@@ -61,14 +70,28 @@ class TestSpace3D(unittest.TestCase):
 
 	def setUp(self):
 		self.space = Cuboid3D(2,3,2)
+
+	def test_size(self):	
+		"""Test that size of space is correct"""
+		self.assertEqual(self.space.size, 12)
 	
 	def test_pointFrom1D(self):	
 		"""Test that 1D point maps correctly to a 3D point in the space"""
 		self.assertEqual(self.space.pointFrom1D(14), Point3D(0,1,2))
 
-	def test_distance(self):	
+	def test_distance3D(self):	
 		"""Test that distance between two points is correct"""
-		self.assertEqual(self.space.distance(10, 3), math.sqrt(3))
+		i = self.space.pointFrom1D(10)
+		j = self.space.pointFrom1D(3)
+		self.assertEqual(distance3D(i, j), math.sqrt(3))
+
+	def test_distance3DWithOffset(self):	
+		"""Test that distance between two points in two model spaces is correct"""
+		offset = Point3D(5,0,0) 
+		adjacent = Cuboid3D(2,3,2, offset=offset)
+		i = self.space.pointFrom1D(10)
+		j = adjacent.pointFrom1D(3)
+		self.assertEqual(distance3D(i, j), math.sqrt(38))
 
 if __name__ == '__main__':
 	unittest.main()
